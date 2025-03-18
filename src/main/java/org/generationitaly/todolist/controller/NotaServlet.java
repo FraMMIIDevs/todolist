2eb284c6e5d0619fb266e802464aceb70b157867
beatr <beatr@LAPTOP-FOU0LC4I.homenet.telecomitalia.it>
2025-03-18 16:46:13 +0100

    servlet aggiornato

--- src/main/java/org/generationitaly/todolist/controller/NotaServlet.java
+++
@@ -1,8 +0,0 @@
-package org.generationitaly.todolist.controller;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-@WebServlet ("/tasks")
-public class NotaServlet extends HttpServlet {
-
-}
---
+++ src/main/java/org/generationitaly/todolist/controller/SaveNotaServlet.java
@@ -0,0 +1,58 @@
+package org.generationitaly.todolist.controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.sql.Time;
+
+import org.generationitaly.todolist.entity.Nota;
+import org.generationitaly.todolist.repository.NotaRepository;
+import org.generationitaly.todolist.repository.impl.NotaRepositoryImpl;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/crea-note")
+public class SaveNotaServlet extends HttpServlet {
+
+	private NotaRepository notaRepository = NotaRepositoryImpl.getInstance();
+
+	/*- private ContattoRepository contattoRepository = ContattoRepositoryImpl.getInstance(); */
+	public SaveNotaServlet() {
+		super();
+		System.out.println("constructor 'SaveNotaServlet()' invoked!");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("method 'SaveNotaServlet.doGet(request, response)' invoked!");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("method 'SaveNotaServlet.doPost(request, response)' invoked!");
+		/*- <input id="input-nome" name="nome" type="text" placeholder="Mario"> */
+		String titolo = request.getParameter("corpoTesto");
+		/*- <input id="input-cognome" name="cognome" type="text" placeholder="Rossi"> */
+		String completata = request.getParameter("completata");
+
+		System.out.println("param 'titolo': " + titolo);
+		System.out.println("param 'completata': " + completata);
+
+		Nota nota = new Nota();
+		nota.setCorpoTesto(titolo);
+		nota.setCompletata(completata);
+
+		notaRepository.save(nota);
+		response.sendRedirect("nota");
+	}
+
+}
